e2b06a9922f7187ea07abeb4b5c7e6aa3457205f
aaya-ahmed <aaya7098@gmail.com>
2025-06-12 13:11:19 +0300

    Refactor styles and structure for menu and order sections; enhance responsiveness and improve layout

--- js/script.js
+++ js/script.js
@@ -1,41 +1,41 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js';
 import { getDatabase, ref, onValue } from 'https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js';
 const firebaseConfig = {
-    apiKey: "AIzaSyArQxe4gqsD1AjQjQ50bqeNHcnpbcwVQIQ",
-    authDomain: "resturant-62854.firebaseapp.com",
-    databaseURL: "https://resturant-62854-default-rtdb.firebaseio.com",
-    projectId: "resturant-62854",
-    storageBucket: "resturant-62854.appspot.com",
-    messagingSenderId: "719599328677",
-    appId: "1:719599328677:web:7cd8a54b9658716f81dc4c",
-    measurementId: "G-ZNC81CREWE"
+  apiKey: "AIzaSyArQxe4gqsD1AjQjQ50bqeNHcnpbcwVQIQ",
+  authDomain: "resturant-62854.firebaseapp.com",
+  databaseURL: "https://resturant-62854-default-rtdb.firebaseio.com",
+  projectId: "resturant-62854",
+  storageBucket: "resturant-62854.appspot.com",
+  messagingSenderId: "719599328677",
+  appId: "1:719599328677:web:7cd8a54b9658716f81dc4c",
+  measurementId: "G-ZNC81CREWE"
 };
-const app=initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 // Initialize Realtime Database
 const db = getDatabase(app);
 // Function to get categories from Firebase Realtime Database
-const getCategories=()=>{
-    const categoryRef = ref(db, 'category');
-    onValue(categoryRef, (snapshot) => {
-        const data = snapshot.val();
-        if (data) {
-            getfoodItems(Object.keys(data)[0]);
-            Object.keys(data).forEach(key => {
-                const categoryItem = document.createElement('button');
-                categoryItem.textContent = data[key].name;
-                categoryItem.addEventListener('click', (e) => {
-                    getfoodItems(key);
-                });
-                const li=document.createElement('li')
-                li.appendChild(categoryItem)
-                document.querySelector('.categories-container ul').appendChild(li);
-            });
-        } else {
-            console.log('No categories found');
-        }
-    }, (error) => {
-        console.error('Error reading data:', error);
-    });
+const getCategories = () => {
+  const categoryRef = ref(db, 'category');
+  onValue(categoryRef, (snapshot) => {
+    const data = snapshot.val();
+    if (data) {
+      getfoodItems(Object.keys(data)[0]);
+      Object.keys(data).forEach(key => {
+        const categoryItem = document.createElement('button');
+        categoryItem.textContent = data[key].name;
+        categoryItem.addEventListener('click', (e) => {
+          getfoodItems(key);
+        });
+        const li = document.createElement('li')
+        li.appendChild(categoryItem)
+        document.querySelector('.categories-container ul').appendChild(li);
+      });
+    } else {
+      console.log('No categories found');
+    }
+  }, (error) => {
+    console.error('Error reading data:', error);
+  });
 }
 
 const getfoodItems = (categoryKey) => {
@@ -43,18 +43,22 @@ const getfoodItems = (categoryKey) => {
   onValue(foodItemsRef, (snapshot) => {
     const data = snapshot.val();
     const container = document.querySelector('.menu-items-container');
-    container.innerHTML = ''; // Clear existing items
-
+    container.innerHTML = '';
     if (data) {
       const fragment = document.createDocumentFragment();
       Object.values(data).forEach(item => {
         const div = document.createElement('div');
         div.classList.add('food-item');
         div.innerHTML = `
+        <div class="food-image">
           <img src="${item.photo}" alt="${item.name}">
-          <h3>${item.name}</h3>
-          <p>${item.description}</p>
-          <span>Price: <strong>$${item.price}</strong></span>
+        </div>
+          <div class="food-details">
+          
+            <h3>${item.name}</h3>
+            <p>${item.description}</p>
+            <span class="price">$${item.price}</span>
+            </div>
         `;
         fragment.appendChild(div);
       });
